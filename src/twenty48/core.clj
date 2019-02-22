c12acafd2840ae5c85255ab11b032c3d0882ee6f
Madhuri Kondekar <in-madhurk@Madhuris-MBP.Dlink>
2019-02-23 00:53:20 +0530

    added functionality to move grid left

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -1,15 +1,28 @@
 (ns twenty48.core
   (:gen-class))
 
+(def partition-as-set-of-two (partial partition-all 2))
+
+(def add-two-numbers (partial apply +))
+(def add-set-of-two (partial map add-two-numbers))
+(defn make-set-of-four [coll] (take 4 (flatten (conj coll (repeat 4 0)))))
+
+(def remove-zeros&-partition-by-identity (comp (partial partition-by identity) (partial remove zero?)))
+(def partition-as-set-of-two-in-one-list (comp (partial apply concat) (partial map partition-as-set-of-two) (partial remove-zeros&-partition-by-identity)))
+(def add-numbers (comp (partial add-set-of-two) (partial partition-as-set-of-two-in-one-list)))
+(def get-final-set (comp (partial make-set-of-four) (partial into []) (partial add-numbers)))
+
+
 (defn move-grid-right
   "Moves an entire grid to the right"
   [grid]
-  grid)
+  )
+  
 
 (defn move-grid-left
   "Moves an entire grid to the left"
   [grid]
-  grid)
+  (map get-final-set grid))
 
 (defn move-grid-down
   "Moves an entire grid down"
