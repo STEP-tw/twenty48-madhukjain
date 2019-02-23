a3344b377ddc295148f2511fea11bc9a91339b4b
Madhuri Kondekar <in-madhurk@Madhuris-MacBook-Pro.local>
2019-02-23 13:05:11 +0530

    Removed unnecessary functions and refactored some functions.

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -2,42 +2,35 @@
   (:gen-class))
 
 (def add (partial map (partial apply +)))
-(defn add-zeros-at-last [coll] (take 4 (flatten (conj coll (repeat 4 0)))))
-(defn add-zeros-at-start [coll] (take-last 4 (flatten (conj [(repeat 4 0)] coll))))
-
-(def remove-zeros&-partition-by-identity (comp (partial partition-by identity) (partial remove zero?)))
-(def group-into-pair (comp 
-                      (partial apply concat)
-                      (partial map (partial partition-all 2))
-                      (partial remove-zeros&-partition-by-identity)))
-
+(defn add-zeros [coll] (take 4 (flatten (conj coll (repeat 4 0)))))
+(def group-by-identity (partial partition-by identity))
+(def group-into-pair (comp (partial apply concat) 
+                           (partial map (partial partition-all 2)) 
+                           (partial group-by-identity)))
 (def add-all (comp (partial add) (partial group-into-pair)))
-
-(def get-final-set (comp 
-                    (partial add-zeros-at-last) 
+(def move-grid (comp 
+                    (partial add-zeros) 
                     (partial into []) 
-                    (partial add-all)))
-(def get-final-for-right-shift (comp 
-                                (partial add-zeros-at-start) 
-                                (partial into []) 
-                                (partial add-all)))
-
-(def move-grid-right
-  "Moves an entire grid to the right"
-  (partial map get-final-for-right-shift))
+                    (partial add-all) (partial remove zero?)))
 
 (def move-grid-left
   "Moves an entire grid to the left"
-  (partial map get-final-set))
+  (partial map move-grid))
+
+(def move-grid-right
+  "Moves an entire grid to the right"
+  (comp (partial map reverse)
+        (partial move-grid-left)
+        (partial map reverse)))
 
 (def move-grid-down
   "Moves an entire grid down"
   (comp (partial apply map vector) 
-        (partial map get-final-for-right-shift) 
+        (partial move-grid-right) 
         (partial apply map vector)))
 
 (def move-grid-up
   "Moves an entire grid up"
   (comp (partial apply map vector) 
-        (partial map get-final-set) 
+        (partial move-grid-left) 
         (partial apply map vector)))
