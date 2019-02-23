ed8ef2bf3d639c964c2b7cf3a66ed06d8f574e40
Madhuri Kondekar <in-madhurk@Madhuris-MacBook-Pro.local>
2019-02-23 11:14:14 +0530

    added functionality to move grid right

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -6,18 +6,23 @@
 (def add-two-numbers (partial apply +))
 (def add-set-of-two (partial map add-two-numbers))
 (defn make-set-of-four [coll] (take 4 (flatten (conj coll (repeat 4 0)))))
+(make-set-of-four [4 2 8])
 
 (def remove-zeros&-partition-by-identity (comp (partial partition-by identity) (partial remove zero?)))
 (def partition-as-set-of-two-in-one-list (comp (partial apply concat) (partial map partition-as-set-of-two) (partial remove-zeros&-partition-by-identity)))
 (def add-numbers (comp (partial add-set-of-two) (partial partition-as-set-of-two-in-one-list)))
 (def get-final-set (comp (partial make-set-of-four) (partial into []) (partial add-numbers)))
 
+(defn add-zeros [coll] (take-last 4 (flatten (conj [(repeat 4 0)] coll))))
+(def get-final-for-right-shift (comp (partial add-zeros) (partial into []) (partial add-numbers)))
+
+
 
 (defn move-grid-right
   "Moves an entire grid to the right"
   [grid]
+  (map get-final-for-right-shift grid)
   )
-  
 
 (defn move-grid-left
   "Moves an entire grid to the left"
