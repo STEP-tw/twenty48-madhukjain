cd4c609c3cfd7151b2f19d1fbab86cb23e6ffbc9
Madhuri Kondekar <in-madhurk@Madhuris-MacBook-Pro.local>
2019-02-23 12:31:41 +0530

    Refactored some function names and modified some functions

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -1,24 +1,30 @@
 (ns twenty48.core
   (:gen-class))
 
-(def partition-as-set-of-two (partial partition-all 2))
-
-(def add-two-numbers (partial apply +))
-(def add-set-of-two (partial map add-two-numbers))
-(defn make-set-of-four [coll] (take 4 (flatten (conj coll (repeat 4 0)))))
+(def add (partial map (partial apply +)))
+(defn add-zeros-at-last [coll] (take 4 (flatten (conj coll (repeat 4 0)))))
+(defn add-zeros-at-start [coll] (take-last 4 (flatten (conj [(repeat 4 0)] coll))))
 
 (def remove-zeros&-partition-by-identity (comp (partial partition-by identity) (partial remove zero?)))
-(def partition-as-set-of-two-in-one-list (comp (partial apply concat) (partial map partition-as-set-of-two) (partial remove-zeros&-partition-by-identity)))
-(def add-numbers (comp (partial add-set-of-two) (partial partition-as-set-of-two-in-one-list)))
-(def get-final-set (comp (partial make-set-of-four) (partial into []) (partial add-numbers)))
+(def group-into-pair (comp 
+                      (partial apply concat)
+                      (partial map (partial partition-all 2))
+                      (partial remove-zeros&-partition-by-identity)))
 
-(defn add-zeros [coll] (take-last 4 (flatten (conj [(repeat 4 0)] coll))))
-(def get-final-for-right-shift (comp (partial add-zeros) (partial into []) (partial add-numbers)))
+(def add-all (comp (partial add) (partial group-into-pair)))
+
+(def get-final-set (comp 
+                    (partial add-zeros-at-last) 
+                    (partial into []) 
+                    (partial add-all)))
+(def get-final-for-right-shift (comp 
+                                (partial add-zeros-at-start) 
+                                (partial into []) 
+                                (partial add-all)))
 
 (def move-grid-right
   "Moves an entire grid to the right"
-  (partial map get-final-for-right-shift)
-  )
+  (partial map get-final-for-right-shift))
 
 (def move-grid-left
   "Moves an entire grid to the left"
@@ -26,8 +32,12 @@
 
 (def move-grid-down
   "Moves an entire grid down"
-  (comp (partial apply map vector) (partial map get-final-for-right-shift) (partial apply map vector)))
+  (comp (partial apply map vector) 
+        (partial map get-final-for-right-shift) 
+        (partial apply map vector)))
 
 (def move-grid-up
   "Moves an entire grid up"
-  (comp (partial apply map vector) (partial map get-final-set) (partial apply map vector)))
+  (comp (partial apply map vector) 
+        (partial map get-final-set) 
+        (partial apply map vector)))
